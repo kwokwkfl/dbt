f6dd1afe967308e8979d946e7c47c3718e4f4cd9
Will Leung <williamleung1392@yahoo.co.uk>
2023-01-03 00:20:52 +0000

    update

--- models/staging/stg_uk_prop_price_data.sql
+++ models/staging/stg_uk_prop_price_data.sql
@@ -1,5 +1,24 @@
 {{ config(materialized='view') }}
 
-select * FROM {{ source('staging','pp_1995') }}
-limit 100 
+select
+id,
+price,
+date(date_of_transfer) as date_of_transfer,
+postcode,
+property_type,
+old_new,
+duration, 
+PAON, 
+SAON,
+street,
+locality,
+town_city
+district,
+county, 
+PPD_category,
+record_status
+ FROM {{ source('staging', 'pp_1995')}}
+-- limit 100 
+
+
 
